bc7d52c3b85287bec15550ebe1107a6908585485
Christopher Iman <chrisiman53@gmail.com>
2020-09-20 17:26:24 -0700

    Made window appear in the middle of the screen

--- Florencia-Engine/src/Platform/Windows/Wwindow.cpp
+++ Florencia-Engine/src/Platform/Windows/Wwindow.cpp
@@ -26,20 +26,27 @@ namespace Florencia {
 		wc.cbClsExtra = 0;
 		wc.cbWndExtra = 0;
 		wc.hInstance = instance;
-		wc.hIcon = nullptr; /*LoadIcon(instance, IDI_APPLICATION);*/
-		wc.hCursor = nullptr;/*LoadCursor(instance, IDC_ARROW);*/
+		wc.hIcon = nullptr;
+		wc.hCursor = nullptr;
 		wc.hbrBackground = nullptr;
 		wc.lpszMenuName = nullptr;
 		wc.lpszClassName = "Window";
 		wc.hIconSm = nullptr;
 		RegisterClassEx(&wc);
 
-		m_Handle = CreateWindowEx(0,"Window",&props.name[0],WS_MINIMIZEBOX|WS_CAPTION|WS_SYSMENU|WS_VISIBLE,0,0,props.width,props.height,0,0,instance,0);
+		RECT desktop = {0};
+		HWND size = GetDesktopWindow();
+		GetWindowRect(size, &desktop);
+
+		m_Handle = CreateWindowEx(
+			NULL, "Window", &props.name[0],
+			WS_MAXIMIZEBOX|WS_MINIMIZEBOX|WS_CAPTION|WS_SYSMENU|WS_VISIBLE,
+			(desktop.right - props.width) / 2, (desktop.bottom - props.height) / 2, props.width, props.height,
+			nullptr, nullptr, instance, 0);
 	}
 
 	void Wwindow::OnUpdate() {
-		MSG m_Message;
-		SecureZeroMemory(&m_Message, sizeof(MSG));
+		MSG m_Message = {0};
 		GetMessage(&m_Message, 0, 0, 0);
 		TranslateMessage(&m_Message);
 		DispatchMessage(&m_Message);
