f057cfca1cd7665df9e5d63a8bf732c24c0c0089
Christopher Iman <chrisiman53@gmail.com>
2021-09-12 10:10:58 -0700

    Decided to get rid of premake

--- Dependencies.lua
+++
@@ -1,5 +0,0 @@
--- Dependencies
-
-IncludeDir = {}
-IncludeDir["glfw"] = "%{wks.location}\\Florencia-Engine\\vendor\\glfw\\include"
-IncludeDir["glad"] = "%{wks.location}\\Florencia-Engine\\vendor\\glad\\include"
