3cf0b73f192f95536086525024f6338d1095246e
Awesomeness211i <chrisiman53@gmail.com>
2023-06-27 18:38:03 -0700

    more testing of ash vulkan api

--- FlorenciaEngine/src/renderer.rs
+++ FlorenciaEngine/src/renderer.rs
@@ -3,30 +3,30 @@ use ash::{
 	vk,
 };
 use winit::{
-	event_loop::EventLoop,
-	window::WindowBuilder,
+	event_loop,
+	window,
 };
 
 use super::Result;
 
 pub fn test() -> Result<()> {
-	let eventLoop = EventLoop::new();
-	let window = WindowBuilder::new().with_title("TEST").with_inner_size(winit::dpi::LogicalSize::new(1920, 1080)).build(&eventLoop)?;
+	let eventLoop = event_loop::EventLoop::new();
+	let window = window::WindowBuilder::new()
+		.with_title("TEST")
+		.with_inner_size(winit::dpi::LogicalSize::new(1920, 1080))
+		.build(&eventLoop)?;
 	let entry = unsafe { ash::Entry::load() }?;
 	let appName = unsafe { std::ffi::CStr::from_bytes_with_nul_unchecked(b"TEST\0") };
 	let layerNames = [
 		unsafe { std::ffi::CStr::from_bytes_with_nul_unchecked(b"VK_LAYER_KHRONOS_validation\0") },
 	];
 	let layerNamesRaw: Vec<_> = layerNames.iter().map(|raw| { raw.as_ptr() }).collect();
-	let applicationInfo = vk::ApplicationInfo {
-		s_type: vk::StructureType::APPLICATION_INFO,
-		p_next: std::ptr::null(),
-		p_application_name: std::ptr::null(),
-		application_version: 0,
-		p_engine_name: std::ptr::null(),
-		engine_version: 0,
-		api_version: vk::API_VERSION_1_3,
-	};
+	let applicationInfo = vk::ApplicationInfo::builder()
+		.application_name(appName)
+		.application_version(0)
+		.engine_name(appName)
+		.engine_version(0)
+		.api_version(vk::API_VERSION_1_3);
 	let instanceCreateInfo = vk::InstanceCreateInfo::builder()
 		.application_info(&applicationInfo)
 		.enabled_layer_names(&layerNamesRaw);
@@ -36,33 +36,24 @@ pub fn test() -> Result<()> {
 		.message_type(vk::DebugUtilsMessageTypeFlagsEXT::GENERAL | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION | vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE);
 	let physicalDevice = unsafe { instance.enumerate_physical_devices() }?[0];
 	let queueFamilyProperties = unsafe { instance.get_physical_device_queue_family_properties(physicalDevice) };
-	let deviceCreateInfo = vk::DeviceCreateInfo {
-		s_type: vk::StructureType::DEVICE_CREATE_INFO,
-		p_next: std::ptr::null(),
-		flags: vk::DeviceCreateFlags::default(),
-		queue_create_info_count: 0,
-		p_queue_create_infos: std::ptr::null(),
-		enabled_extension_count: 0,
-		pp_enabled_extension_names: std::ptr::null(),
-		p_enabled_features: std::ptr::null(),
-		/* This done to avoid implementing deprecated options and also warnings that come with that */
-		..Default::default()
-	};
+	let deviceQueuePriorities = [
+		1.0,
+	];
+	let deviceQueueCreateInfos = [
+		vk::DeviceQueueCreateInfo::builder()
+			.queue_family_index(0)
+			.queue_priorities(&deviceQueuePriorities)
+			.build(),
+	];
+	let deviceCreateInfo = vk::DeviceCreateInfo::builder().queue_create_infos(&deviceQueueCreateInfos);
 	let device = unsafe { instance.create_device(physicalDevice, &deviceCreateInfo, None) }?;
-	let commandPoolCreateInfo = vk::CommandPoolCreateInfo {
-		s_type: vk::StructureType::COMMAND_POOL_CREATE_INFO,
-		p_next: std::ptr::null(),
-		flags: vk::CommandPoolCreateFlags::default(),
-		queue_family_index: 0,
-	};
+	let commandPoolCreateInfo = vk::CommandPoolCreateInfo::builder().queue_family_index(0);
 	let commandPool = unsafe { device.create_command_pool(&commandPoolCreateInfo, None) }?;
-	let commandBufferAllocateInfo = vk::CommandBufferAllocateInfo {
-		s_type: vk::StructureType::COMMAND_BUFFER_ALLOCATE_INFO,
-		p_next: std::ptr::null(),
-		command_pool: commandPool,
-		level: vk::CommandBufferLevel::PRIMARY,
-		command_buffer_count: 0,
-	};
+	let commandBufferAllocateInfo = vk::CommandBufferAllocateInfo::builder()
+		.command_pool(commandPool)
+		.level(vk::CommandBufferLevel::PRIMARY)
+		.command_buffer_count(1)
+		.build();
 	let commandBuffers = unsafe { device.allocate_command_buffers(&commandBufferAllocateInfo) }?;
 	Ok(())
 }
