f703d0a67c4d7e47b8fb5396b50448d67e38cdbb
Awesomeness211i <chrisiman53@gmail.com>
2023-08-22 14:49:21 -0700

    trying to figure out ash

--- FlorenciaEngine/src/renderer.rs
+++ FlorenciaEngine/src/renderer.rs
@@ -1,41 +1,79 @@
 use ash::{
 	self,
 	vk,
+	extensions::{
+		ext,
+		khr,
+	},
 };
+
 use winit::{
-	event_loop,
-	window,
+	event_loop::EventLoop,
+	window::{Window, WindowBuilder},
 };
 
 use super::Result;
 
+unsafe extern "system" fn vulkanDebugUtilsCallback(message_severity: vk::DebugUtilsMessageSeverityFlagsEXT, message_type: vk::DebugUtilsMessageTypeFlagsEXT, p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT, _p_user_data: *mut std::ffi::c_void) -> vk::Bool32 {
+	let message = std::ffi::CStr::from_ptr((*p_callback_data).p_message);
+	let severity = format!("{:?}", message_severity).to_lowercase();
+	let ty = format!("{:?}", message_type).to_lowercase();
+	println!("[Debug][{}][{}] {:?}", severity, ty, message);
+	vk::FALSE
+}
+
 pub fn test() -> Result<()> {
-	let eventLoop = event_loop::EventLoop::new();
-	let window = window::WindowBuilder::new()
+	let eventLoop = EventLoop::new();
+	let window = WindowBuilder::new()
 		.with_title("TEST")
 		.with_inner_size(winit::dpi::LogicalSize::new(1920, 1080))
 		.build(&eventLoop)?;
-	let entry = unsafe { ash::Entry::load() }?;
-	let appName = unsafe { std::ffi::CStr::from_bytes_with_nul_unchecked(b"TEST\0") };
 	let layerNames = [
-		unsafe { std::ffi::CStr::from_bytes_with_nul_unchecked(b"VK_LAYER_KHRONOS_validation\0") },
+		std::ffi::CString::new("VK_LAYER_KHRONOS_validation")?,
 	];
 	let layerNamesRaw: Vec<_> = layerNames.iter().map(|raw| { raw.as_ptr() }).collect();
+	let mut extensions = vec![
+		ext::DebugUtils::name().as_ptr(),
+		khr::Surface::name().as_ptr(),
+	];
+	// for extensionName in ash_window::enumerate_required_extensions(&window)? {
+	// 	extensions.push(extensionName.as_ptr());
+	// }
+
+	let entry = unsafe { ash::Entry::load() }?;
+	let appName = std::ffi::CString::new("App Name")?;
+	let engineName = std::ffi::CString::new("Engine Name")?;
 	let applicationInfo = vk::ApplicationInfo::builder()
-		.application_name(appName)
+		.application_name(&appName)
 		.application_version(0)
-		.engine_name(appName)
+		.engine_name(&engineName)
 		.engine_version(0)
 		.api_version(vk::API_VERSION_1_3);
 	let instanceCreateInfo = vk::InstanceCreateInfo::builder()
 		.application_info(&applicationInfo)
 		.enabled_layer_names(&layerNamesRaw);
 	let instance = unsafe { entry.create_instance(&instanceCreateInfo, None) }?;
+
+	// let surface = unsafe {
+	// 	ash_window::create_surface(&entry, &instance, &window, &window, None)
+	// }?;
+	let surfaceLoader = khr::Surface::new(&entry, &instance);
+
+	let debugUtils = ext::DebugUtils::new(&entry, &instance);
 	let debugInfo = vk::DebugUtilsMessengerCreateInfoEXT::builder()
-		.message_severity(vk::DebugUtilsMessageSeverityFlagsEXT::ERROR | vk::DebugUtilsMessageSeverityFlagsEXT::WARNING | vk::DebugUtilsMessageSeverityFlagsEXT::INFO)
-		.message_type(vk::DebugUtilsMessageTypeFlagsEXT::GENERAL | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION | vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE);
+		.message_severity(vk::DebugUtilsMessageSeverityFlagsEXT::ERROR | vk::DebugUtilsMessageSeverityFlagsEXT::WARNING | vk::DebugUtilsMessageSeverityFlagsEXT::INFO | vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE)
+		.message_type(vk::DebugUtilsMessageTypeFlagsEXT::GENERAL | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION | vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE)
+		.pfn_user_callback(Some(vulkanDebugUtilsCallback));
+	// let debugMessenger = unsafe {
+	// 	debugUtils.create_debug_utils_messenger(&debugInfo, None)
+	// }?;
+
 	let physicalDevice = unsafe { instance.enumerate_physical_devices() }?[0];
+	
+	// let formats = unsafe { surfaceLoader.get_physical_device_surface_formats(physicalDevice, surface) };
+
 	let queueFamilyProperties = unsafe { instance.get_physical_device_queue_family_properties(physicalDevice) };
+
 	let deviceQueuePriorities = [
 		1.0,
 	];
@@ -52,93 +90,7 @@ pub fn test() -> Result<()> {
 	let commandBufferAllocateInfo = vk::CommandBufferAllocateInfo::builder()
 		.command_pool(commandPool)
 		.level(vk::CommandBufferLevel::PRIMARY)
-		.command_buffer_count(1)
-		.build();
+		.command_buffer_count(1);
 	let commandBuffers = unsafe { device.allocate_command_buffers(&commandBufferAllocateInfo) }?;
 	Ok(())
-}
-
-pub struct Instance {
-	entry: ash::Entry,
-	instance: ash::Instance,
-	allocationCallbacks: Option<vk::AllocationCallbacks>,
-}
-
-pub struct InstanceInfo {
-	appInfo: vk::ApplicationInfo,
-	extensions: Vec<std::ffi::CString>,
-	extensionPointers: Vec<*const i8>,
-	instanceInfo: vk::InstanceCreateInfo,
-	allocationCallbacks: Option<vk::AllocationCallbacks>,
-}
-
-impl InstanceInfo {
-	pub fn new(requiredExtensions: Vec<String>, allocationCallbacks: Option<vk::AllocationCallbacks>) -> Self {
-		let appInfo = vk::ApplicationInfo {
-			s_type: vk::StructureType::APPLICATION_INFO,
-			p_next: std::ptr::null(),
-			p_application_name: std::ptr::null(),
-			application_version: 0,
-			p_engine_name: std::ptr::null(),
-			engine_version: 0,
-			api_version: vk::API_VERSION_1_3,
-		};
-		let extensions: Vec<_> = requiredExtensions.iter().map(|ext| { std::ffi::CString::new(ext.clone()).expect("Failed to convert extension name") }).collect();
-		let extensionPointers: Vec<_> = extensions.iter().map(|ext| { ext.as_ptr() }).collect();
-		let instanceInfo = vk::InstanceCreateInfo {
-			s_type: vk::StructureType::INSTANCE_CREATE_INFO,
-			p_next: std::ptr::null(),
-			flags: vk::InstanceCreateFlags::empty(),
-			p_application_info: &appInfo,
-			enabled_layer_count: 0,
-			pp_enabled_layer_names: std::ptr::null(),
-			enabled_extension_count: extensionPointers.len() as _,
-			pp_enabled_extension_names: extensionPointers.as_ptr(),
-		};
-		Self {
-			appInfo,
-			extensions,
-			extensionPointers,
-			instanceInfo,
-			allocationCallbacks,
-		}
-	}
-}
-
-impl Instance {
-	pub fn new(entry: ash::Entry, info: InstanceInfo) -> Result<Self> {
-		let instance = unsafe { entry.create_instance(&info.instanceInfo, info.allocationCallbacks.as_ref()) }?;
-		Ok(Self {
-			entry,
-			instance,
-			allocationCallbacks: info.allocationCallbacks
-		})
-	}
-
-	pub fn handle(self: &Self) -> vk::Instance {
-		self.instance.handle()
-	}
-
-	fn Data(self: &Self) -> (&ash::Instance, &Option<vk::AllocationCallbacks>) {
-		(&self.instance, &self.allocationCallbacks)
-	}
-}
-
-impl Drop for Instance {
-	fn drop(self: &mut Self) {
-		unsafe { self.instance.destroy_instance(self.allocationCallbacks.as_ref()) };
-	}
-}
-
-pub struct PhysicalDevicePool {
-	pool: Vec<vk::PhysicalDevice>,
-}
-
-impl PhysicalDevicePool {
-	fn new(instance: &Instance) -> Result<Self> {
-		let (i, _) = instance.Data();
-		Ok(Self {
-			pool: unsafe { i.enumerate_physical_devices() }?,
-		})
-	}
 }
